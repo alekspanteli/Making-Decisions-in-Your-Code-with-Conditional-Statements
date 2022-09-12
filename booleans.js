b113ff53fcd5436e101a0a44177063b976230d43
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-09-12 12:32:15 +0300

    2 files modified, 1 added

--- booleans.js
+++ booleans.js
@@ -1,16 +1,16 @@
-let correctGuess = false;
-const number = 2;
-const guess = prompt("Give a number")
+// let correctGuess = false;
+// const number = 2;
+// const guess = prompt("Give a number")
 
-if (+guess === number){
-  correctGuess = true;
-}
+// if (+guess === number){
+//   correctGuess = true;
+// }
 
 
 
-if (correctGuess === true){
-  console.log("This is True")
-}
-else{
-  console.log(`This is False. The number was ${number}`)
-}
+// if (correctGuess === true){
+//   console.log("This is True")
+// }
+// else{
+//   console.log(`This is False. The number was ${number}`)
+// }
---
+++ else-if.js
@@ -0,0 +1,12 @@
+weather = "";
+
+if ( weather === "sun"){
+ console.log("Its sunny");
+}
+else if (weather === "rain"){
+  console.log("Its raining");
+} else if(weather === "snow"){
+  console.log("Its snowing");
+} else{
+  console.log("Whats up");
+}
